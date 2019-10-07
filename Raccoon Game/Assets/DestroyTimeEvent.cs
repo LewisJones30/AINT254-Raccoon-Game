543114091c7007e219aa9cf32b1b65e4528d25d0
LewisJones30 <43572935+LewisJones30@users.noreply.github.com>
2019-10-07 15:37:39 +0100

    Objects Spawning
    The cube (current placeholder) is now able to spawn a placeholder sphere at a time that can be set by the user.

---
+++ Raccoon Game/Assets/DestroyTimeEvent.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class DestroyTimeEvent : MonoBehaviour
+{
+
+    public float time = 1;
+    public bool repeat = false;
+    private void Start()
+    {
+        if (repeat)
+        {
+            InvokeRepeating("DestroyEvent", 0, time);
+        }
+        else
+        {
+            Invoke("DestroyEvent", time);
+        }
+    }
+    private void DestroyEvent()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Raccoon Game/Assets/SpawnObjects.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObjects : MonoBehaviour
+{
+
+    public GameObject prefabtoSpawn;
+
+
+    public void Spawn()
+    {
+        Instantiate(prefabtoSpawn, transform.position, transform.rotation);
+        prefabtoSpawn.GetComponent<Rigidbody>().velocity = new Vector3(15000, 0, 0);
+    }
+}
