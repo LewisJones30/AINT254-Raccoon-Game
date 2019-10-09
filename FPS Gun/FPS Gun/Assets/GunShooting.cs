d16812b86fa7ac63fdacff7587910291dda1f65c
LewisJones30 <43572935+LewisJones30@users.noreply.github.com>
2019-10-09 16:21:59 +0100

    Adding Wrecking Ball & FPS Gun
    Two unity projects imported, using a Wrecking Ball and FPS gun to demonstrate certain aspects of game

---
+++ FPS Gun/FPS Gun/Assets/GunShooting.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunShooting : MonoBehaviour
+{
+
+    public Rigidbody bullet;
+    private int bulletDelay = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            bulletDelay = bulletDelay + 1;
+            if (bulletDelay == 10)
+            {
+                Rigidbody clone;
+                clone = Instantiate(bullet, transform.position, transform.rotation);
+                clone.velocity = transform.TransformDirection(Vector3.up * 50);
+
+                bulletDelay = 0;
+            }
+
+        }
+        else
+        {
+
+        }
+    }
+}
