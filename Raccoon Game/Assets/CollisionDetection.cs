4810212bc46f44a9469045c871b5cb768f7f9199
LewisJones30 <43572935+LewisJones30@users.noreply.github.com>
2019-10-11 15:40:25 +0100

    Addition of movement, collision
    Addition of these scripts.
    Camera still needs fixing though - keeps moving even if there's been a collision - eventually loses the player

---
+++ Raccoon Game/Assets/CollisionDetection.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDetection : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.name == "DefaultFood(Clone)")
+        {
+
+            //Collision Detection - Add to score
+            Destroy(other.gameObject);
+        }
+        else if (other.gameObject.name == "DecayedFood(Clone)")
+        {
+            //Collision Detection negative health here
+            Destroy(other.gameObject);
+        }
+    }
+}
--- Raccoon Game/Assets/MovementScript.cs
+++ Raccoon Game/Assets/MovementScript.cs
@@ -6,11 +6,12 @@ public class MovementScript : MonoBehaviour
 {
 
     Rigidbody player;
-    public float speed = 2.0f;
+    public float movementSpeed = 2;
+    public Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = this.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -18,7 +19,26 @@ public class MovementScript : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.UpArrow))
         {
-            transform.position += transform.TransformDirection(Vector3.forward) * Time.deltaTime * speed;
+            rb.MovePosition(transform.position + (Vector3.forward * movementSpeed * Time.deltaTime));
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.MovePosition(transform.position + (Vector3.right * movementSpeed * Time.deltaTime));
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            rb.MovePosition(transform.position + (Vector3.back * movementSpeed * Time.deltaTime));
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.MovePosition(transform.position + (Vector3.left * movementSpeed * Time.deltaTime));
         }
     }
+    //private void OnCollisionEnter(Collision other)
+    //{
+      //  if (other.gameObject.name == "DecayedFood")
+        //    SendMessage("Collision Detected!");
+        //Destroy(other);
+    //}
 }
+
