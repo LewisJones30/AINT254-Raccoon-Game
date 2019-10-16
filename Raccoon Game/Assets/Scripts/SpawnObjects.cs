fcf5e9f6a1ff4abf215a5968d509fe880d28ca09
LewisJones30 <43572935+LewisJones30@users.noreply.github.com>
2019-10-16 17:42:08 +0100

    Addition of basic time mechanic, added some more spawner cubes
    Added a basic time mechanic that give sthe player 60 seconds to get 10 balls.

--- Raccoon Game/Assets/Scripts/SpawnObjects.cs
+++ Raccoon Game/Assets/Scripts/SpawnObjects.cs
@@ -5,12 +5,24 @@ using UnityEngine;
 public class SpawnObjects : MonoBehaviour
 {
 
-    public GameObject prefabtoSpawn;
+    public GameObject healthySpawn, decayedSpawn;
 
 
     public void Spawn()
     {
-        Instantiate(prefabtoSpawn, transform.position, transform.rotation);
-        prefabtoSpawn.GetComponent<Rigidbody>().velocity = new Vector3(15000, 0, 0);
+        int randomInt = 0;
+        randomInt = Random.Range(0, 9);
+        if (randomInt < 5)
+        {
+            Rigidbody rb;
+            Instantiate(healthySpawn, transform.position, transform.rotation);
+            rb = this.GetComponent<Rigidbody>();
+            rb.AddRelativeForce(new Vector3(Random.Range(1, 1500), Random.Range(1, 1500), Random.Range(1, 1500)));
+        }
+        else
+        {
+            Instantiate(decayedSpawn, transform.position, transform.rotation);
+            decayedSpawn.GetComponent<Rigidbody>().AddRelativeForce(new Vector3(Random.Range(1,1500), Random.Range(1,1500), Random.Range(1,1500)));
+        }
     }
 }
---
+++ Raccoon Game/Assets/Scripts/Timer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+    public float timeLeft = 60.0f;
+    public Text timeLeftText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeLeftText.color = Color.green;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        timeLeftText.text = "Time Remaining: " + timeLeft.ToString("F2");
+        if (timeLeft < 40)
+        {
+            timeLeftText.color = Color.yellow;
+            if (timeLeft < 20)
+            {
+                timeLeftText.color = Color.red;
+            }
+        }
+        if (timeLeft < 0)
+        {
+            SceneManager.LoadScene("GameLoseScene");
+        }
+
+    }
+}
