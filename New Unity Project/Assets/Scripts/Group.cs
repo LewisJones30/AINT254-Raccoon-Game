788583d8b8a09b257e6e788087a6f1288d7283d3
LewisJones30 <43572935+LewisJones30@users.noreply.github.com>
2019-10-31 14:09:51 +0000

    Addition of Sound project

---
+++ New Unity Project/Assets/Scripts/Group.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Group : MonoBehaviour
+{
+    private CanvasGroup m_group;
+
+	// Use this for initialization
+	void Start ()
+    {
+        m_group = GetComponent<CanvasGroup>();
+
+        m_group.alpha = 0;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(m_group.alpha == 0)
+            {
+                m_group.alpha = 1;
+            }
+            else
+            {
+                m_group.alpha = 0;
+            }
+        }
+	}
+}
---
+++ Vectors/Assets/Chaser.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chaser : MonoBehaviour
+{
+
+    public float speed = 50.0f;
+    public Transform chase;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 heading = transform.position - chase.position;
+        Vector3 normHeading = Vector3.Normalize(heading);
+        float crproduct = Vector3.Cross(transform.position, normHeading).y;
+        GetComponent<Rigidbody>().AddTorque(new Vector3(0, crproduct, 0));
+
+    }
+}
