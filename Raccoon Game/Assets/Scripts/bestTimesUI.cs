d861057949073e18df13273068a237bd6d0e246f
LewisJones30 <43572935+LewisJones30@users.noreply.github.com>
2019-11-19 16:06:37 +0000

    UI updates, anotehr new script
    Additions:
    New script for best times, similar to GetBestTime, which is now partially obslete.
    Addition of some animations
    New scene for best time added.
    Bug fixes:
    None

---
+++ Raccoon Game/Assets/Scripts/bestTimesUI.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class bestTimesUI : MonoBehaviour
+{
+    public Text level1BestTime, level2BestTime, level3BestTime, level4BestTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        float bestTimeFloat = PlayerPrefs.GetFloat("BestTime");
+        int minutes = Mathf.FloorToInt(bestTimeFloat / 60f);
+        int seconds = Mathf.FloorToInt(bestTimeFloat - minutes * 60);
+        Debug.Log(minutes.ToString());
+        Debug.Log(seconds.ToString());
+        Debug.Log(PlayerPrefs.GetFloat("BestTime").ToString());
+        if (minutes + seconds == 0)
+        {
+            string bestTimeString = "Level 1 best time: N/A. Go set a time!";
+            level1BestTime.text = bestTimeString;
+        }
+        else
+        {
+            string bestTimeString = string.Format("Level 1 best time: " + "{0:00}:{1:00}", minutes, seconds);
+            level1BestTime.text = bestTimeString;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
