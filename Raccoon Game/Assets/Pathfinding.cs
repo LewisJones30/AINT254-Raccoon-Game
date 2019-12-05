0156f36bb809012e663cad3253ecd9c2ea50ccca
LewisJones30 <43572935+LewisJones30@users.noreply.github.com>
2019-12-05 15:45:17 +0000

    Addition of pathfinding, a duplicate raccoon with pathfinding
    Additions:
    New pathfinding script and prefab (copy of the original player raccoon, subject to change)
    New cubes to signify each patrol point. These will be hidden in the demo.
    Bugs found:
    Pathfinding raccoon faces the wrong direction.
    Bugs fixed/updates:
    Raccoon asset has been updated so that there is a singular mesh.

---
+++ Raccoon Game/Assets/Pathfinding.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Pathfinding : MonoBehaviour
+{
+    public Transform[] navigationPoints;
+    private NavMeshAgent nav;
+    private int destinationPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        nav = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (!nav.pathPending && nav.remainingDistance < 1f)
+        {
+            MovePoint();
+        }
+    }
+    void MovePoint()
+    {
+        if (navigationPoints.Length == 0)
+        {
+            return;
+        }
+        nav.destination = navigationPoints[destinationPoint].position;
+        destinationPoint = (destinationPoint + 1) % navigationPoints.Length;
+    }
+}
